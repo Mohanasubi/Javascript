bc03039f456c94f1196250e376f7dd16ca2ea8bf
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-11 14:16:27 +0530

    UPDATE -updated the code

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -1,7 +1,15 @@
 //First javascript file
-const num1 = 5;
+/*const num1 = 5;
 const num2 = 3;
 
 const sum = num1 + num2;
 
 console.log(sum);
+*/
+
+function add(num1,num2)
+{
+    console.log(num1+num2)
+}
+add(3,4)
+
