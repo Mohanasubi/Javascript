5f15e8b33e5c4de5c999118d045a7d18c7e3a188
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-12 11:28:54 +0530

    ADD - added javascript  operator  examples

--- solution.js
+++ solution.js
@@ -31,3 +31,31 @@ let input;
 console.log(typeof(input))
 let input2 = null;
 console.log(input2)
+
+//operators
+const sum = 5 + 3;
+const diff = 10 - 2; 
+const p = 4 * 2; 
+const q = 8 / 2; 
+console.log(sum, diff, p, q);
+
+let n = 10;
+n += 5;
+n *= 2;
+console.log(n);
+
+console.log(10 > 5);
+console.log(10 === "10");
+
+const a = true, b = false;
+console.log(a && b);
+console.log(a || b);
+
+const age = 18;
+const status = age >= 18 ? "Adult" : "Minor";
+console.log(status);
+
+let x = 5;
+console.log(++x); 
+console.log(x--);
+
