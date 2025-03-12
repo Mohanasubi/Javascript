0c39a67a9e2c3cc8880511c96caa6c4a96791f73
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-12 11:22:24 +0530

    ADD - added javascript primitive datatypes examples

---
+++ solution.js
@@ -0,0 +1,33 @@
+var testOne=5;
+testOne = "abc"
+function test(a){
+    console.log(a)
+}
+test(testOne)
+
+
+let testTwo = 5
+testTwo = "xyz"
+function test2(a){
+    console.log(a)
+}
+test2(testTwo)
+
+
+const testThree = 3.14;
+function test3(a){
+    console.log(a)
+}
+test3(testThree)
+
+//primitive datatypes
+let str1 = "subi"
+console.log(typeof(str1))
+const number= 3.14
+console.log(typeof(number))
+let bool = true;
+console.log(typeof(bool))
+let input;
+console.log(typeof(input))
+let input2 = null;
+console.log(input2)
